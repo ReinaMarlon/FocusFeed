13e906d55135665edb504233d26e555a0f1c5cea
ReinaMarlon <reinadinas.marlon@gmail.com>
2026-03-25 11:54:19 -0500

    Initial project structure

---
+++ src/main/java/com/marlonreina/focusfeed/FocusfeedApplication.java
@@ -0,0 +1,13 @@
+package com.marlonreina.focusfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusfeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusfeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marlonreina/focusfeed/FocusfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marlonreina.focusfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusfeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
